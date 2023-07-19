0f96d4e8c11426b729cdadb794fffef9f2d4b966
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-19 22:28:14 +0530

    modified allproperty file

--- src/pages/AllProperties.tsx
+++ src/pages/AllProperties.tsx
@@ -157,7 +157,68 @@ const AllProperties = () => {
           icon={<Add />}
         />
       </Stack>
+
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        {allProperties?.map((property) => (
+          <PropertyCard
+            key={property._id}
+            id={property._id}
+            title={property.title}
+            location={property.location}
+            price={property.price}
+            photo={property.photo}
+          />
+        ))}
       </Box>
+
+      {allProperties.length > 0 && (
+        <Box display="flex" gap={2} mt={3} flexWrap="wrap">
+          <CustomButton
+            title="Previous"
+            handleClick={() => setCurrent((prev) => prev - 1)}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            disabled={!(current > 1)}
+          />
+          <Box
+            display={{ xs: "hidden", sm: "flex" }}
+            alignItems="center"
+            gap="5px"
+          >
+            Page{" "}
+            <strong>
+              {current} of {pageCount}
+            </strong>
+          </Box>
+          <CustomButton
+            title="Next"
+            handleClick={() => setCurrent((prev) => prev + 1)}
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            disabled={current === pageCount}
+          />
+          <Select
+            variant="outlined"
+            color="info"
+            displayEmpty
+            required
+            inputProps={{ "aria-label": "Without label" }}
+            defaultValue={10}
+            onChange={(e) =>
+              setPageSize(
+                e.target.value ? Number(e.target.value) : 10,
+              )
+            }
+          >
+            {[10, 20, 30, 40, 50].map((size) => (
+              <MenuItem key={size} value={size}>
+                Show {size}
+              </MenuItem>
+            ))}
+          </Select>
+        </Box>
+      )}
+    </Box>
   );
 };
 
