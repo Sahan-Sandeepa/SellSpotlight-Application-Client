51a40412a4bb6e8fe71d4cc8fdd5e5a2b3e5a964
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-20 00:54:18 +0530

    implemented my profile section

--- src/pages/My-Profile.tsx
+++ src/pages/My-Profile.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useGetIdentity, useOne } from "@refinedev/core";
+
+import { Profile } from "components";
 
 const MyProfile = () => {
+  const { data: user } = useGetIdentity({
+    v3LegacyAuthProviderCompatible: true,
+  });
+  const { data, isLoading, isError } = useOne({
+    resource: "users",
+    id: user?.userid,
+  });
+
+  const myProfile = data?.data ?? [];
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>error...</div>;
+
   return (
-    <div>My-Profile</div>
-  )
-}
+    <Profile
+      type="My"
+      name={myProfile.name}
+      avatar={myProfile.avatar}
+      email={myProfile.email}
+      properties={myProfile.allProperties}
+    />
+  );
+};
 
 export default MyProfile;
